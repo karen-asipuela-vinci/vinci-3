8e91c3a7daeb335f0af813d841c2f97e948c1951
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-10-15 18:54:44 +0200

    Spring : sem3

---
+++ spring/Amazing/src/main/java/be/vinci/ipl/amazing/AmazingApplication.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.amazing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AmazingApplication.class, args);
+  }
+
+}
---
+++ spring/Amazing/src/test/java/be/vinci/ipl/amazing/AmazingApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.amazing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
